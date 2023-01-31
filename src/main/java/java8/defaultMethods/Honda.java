6f2e45b1ea2478f73c0e57f4cad5b900e194c053
mahesh.thamilarasu <mahesh.thamilarasu@atmecs.com>
2023-01-31 12:58:37 +0530

    Lamda examples commits

---
+++ src/main/java/java8/defaultMethods/Honda.java
@@ -0,0 +1,35 @@
+package java8.defaultMethods;
+
+public class Honda implements Vehicle{
+    @Override // overriding abstract method (mandatory)
+    public int getSpeed() {
+        return 100;
+    }
+
+    @Override // overriding abstract method (mandatory)
+    public void applyBreak() {
+        System.out.println("Break applied...");
+    }
+
+    @Override // overriding a default method (not mandatory)
+    public void autoPilot() {
+        Vehicle.super.autoPilot(); // calling super class default method
+        System.out.println("Honda Implemented auto pilot feature");
+    }
+
+    public static void main(String[] args) {
+        Honda honda = new Honda();
+        System.out.println(honda.getSpeed());
+        honda.applyBreak();
+        honda.autoPilot();
+        Honda.sayHello();
+
+        // calling static method from interface
+        Vehicle.sayHello();
+    }
+
+    // static method inside class
+    public static void sayHello(){
+        System.out.println("Say Hello from Class Honda");
+    }
+}
